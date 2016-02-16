274e48fc4648a9aaeb5a04c2da1bbf28a42bae0c
woodyjr <woodyjr@mail.uc.edu>
2016-02-15 19:14:08 -0500

    Construction of model classes

--- CRMSystem/Class1.cs
+++ CRMSystem/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinalProject.Models
+namespace CRMSystem
 {
-    class Class1
+    public class Class1
     {
     }
 }
